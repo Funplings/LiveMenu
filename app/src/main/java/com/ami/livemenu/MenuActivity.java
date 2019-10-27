a3c4b56ad11c71fcb4fd8d0af27ead8ca695d1a6
Imon Husain <imonh222@berkeley.edu>
2019-10-26 22:59:07 -0700

    ctrl-z abortion

--- app/src/main/java/com/ami/livemenu/MenuActivity.java
+++ app/src/main/java/com/ami/livemenu/MenuActivity.java
@@ -50,4 +50,83 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void processBitmap(Bitmap bitmap){
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                processTextRecognitionResult(texts);
+                            }
+                        }
+                )
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(Exception e) {
+                                // Task failed with an exception
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            Log.i(null, "No text found");
+            return;
+        }
+
+        // Clear items
+        for (Object item: ItemButtons) {
+            layout.removeView((View) item);
+        }
+        ItemButtons.clear();
+
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+
+                    // Get element
+                    FirebaseVisionText.Element elem = elements.get(k);
+                    // Get text
+                    final String rawText = elem.getText();
+
+                    // Get button position/size
+                    Rect textBox = elem.getBoundingBox();
+                    float posX = (float) ((textBox.left + textBox.right) / 2.0);
+                    float posY = (float) ((textBox.top + textBox.bottom) / 2.0);
+                    int width = textBox.width();
+                    int height = textBox.height();
+
+                    // Create button
+                    Button b1 = new Button(this);
+                    b1.setText(rawText);
+                    b1.setX(posX);
+                    b1.setY(posY);
+                    //b1.setLayoutParams(new ConstraintLayout.LayoutParams(width, height));
+
+                    // Let button onClick show images
+                    b1.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            Toast.makeText(getApplicationContext(), rawText, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // Add button to set/layout
+                    ItemButtons.add(b1);
+                    layout.addView(b1);
+                }
+            }
+        }
+    }
+
+
 }
